ae9bc30676f11dd8df334e032103747a272bba72
Shyam Babu <shyambabau1200@gmail.com>
2026-07-18 14:07:15 +0530

    Complete Day 7: Random Forest Regression, Model Comparison and Feature Importance Analysis

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+import matplotlib.pyplot as plt
 # Load Cleaned dataset
 df = pd.read_csv("cleaned_train.csv")
 
@@ -65,3 +67,63 @@ print("\n===== Model Evaluation =====")
 print("MAE :", mae)
 print("RMSE:", rmse)
 print("R2  :", r2)
+
+rf_model = RandomForestRegressor(
+    n_estimators=100,
+    random_state=42
+)
+rf_model.fit(X_train, y_train)
+rf_predictions = rf_model.predict(X_test)
+rf_mae = mean_absolute_error(y_test, rf_predictions)
+
+rf_rmse = np.sqrt(
+    mean_squared_error(y_test, rf_predictions)
+)
+
+rf_r2 = r2_score(y_test, rf_predictions)
+print("\n===== Random Forest Evaluation =====")
+
+print("MAE :", rf_mae)
+print("RMSE:", rf_rmse)
+print("R2  :", rf_r2)
+
+comparison = pd.DataFrame({
+    "Metric": ["MAE", "RMSE", "R2 Score"],
+    "Linear Regression": [mae, rmse, r2],
+    "Random Forest": [rf_mae, rf_rmse, rf_r2]
+})
+
+print("\n===== Model Comparison =====")
+print(comparison)
+
+feature_importance = rf_model.feature_importances_
+
+importance_df = pd.DataFrame({
+    "Feature": X.columns,
+    "Importance": feature_importance
+})
+importance_df = importance_df.sort_values(
+    by="Importance",
+    ascending=False
+)
+print("\n===== Top 10 Important Features =====")
+
+print(importance_df.head(10))
+
+top10 = importance_df.head(10)
+
+plt.figure(figsize=(10,6))
+
+plt.bar(top10["Feature"], top10["Importance"])
+
+plt.title("Top 10 Important Features")
+
+plt.xlabel("Features")
+
+plt.ylabel("Importance")
+
+plt.xticks(rotation=45)
+
+plt.tight_layout()
+
+plt.show()
